d84b27f7b7fd613d5976964be15d1a92f050b9c1
alkaon <alkaonchik@gmail.com>
2020-05-17 15:09:04 +0200

    added projects

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
+
+
+function App() {
+  return (
+    <div className ='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg' />
+      </div>
+      <div>
+        avatar+description
+      </div>
+      <div>
+        My posts
+        <div>
+          new posts
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post1
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
